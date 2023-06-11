6931b35df63f1dc9aa6dd927f0fcdf80fdb0c870
Snoroz <55320908+Snoroz@users.noreply.github.com>
2023-06-11 12:59:12 +0200

    Update sun.py

--- sun.py
+++ sun.py
@@ -1,21 +1,26 @@
-from ursina import Entity
-from panda3d.core import DirectionalLight
+from ursina import Entity, Vec4
+from panda3d.core import DirectionalLight, AmbientLight
 
 class Sun(Entity):
-    def __init__(self, player):
+    def __init__(self, target):
         super().__init__()
 
-        self.lighting = DirectionalLight("lighting")
+        self.sun = DirectionalLight("sun")
 
-        self.lighting.getLens().setNearFar(-120, 300)
-        self.lighting.getLens().setFilmSize((150, 150))
+        self.sun.getLens().setNearFar(-120, 300)
+        self.sun.getLens().setFilmSize((150, 150))
 
-        self.pivot = render.attachNewNode(self.lighting)
+        self.pivot = render.attachNewNode(self.sun)
         self.pivot.lookAt((1,-1,-1))
         render.setLight(self.pivot)
 
-        self.player = player
+        self.lighting = AmbientLight('lighting')
+        self.lighting.setColor(Vec4(0.4, 0.5, 0.6, 0) * 1.3)
+        self.parent = render.attachNewNode(self.lighting)
+        render.setLight(self.parent)
+
+        self.target = target
 
     def update(self):
-        self.lighting.setShadowCaster(True, 4000, 4000)
-        self.pivot.setPos(self.player.world_position)
+        self.sun.setShadowCaster(True, 4000, 4000)
+        self.pivot.setPos(self.target.world_position)
